f7caf4129ad6373db3fc8a5c92b4fa2a2263686a
Ricardo Gonzales Delgado <a2018118765@estudiantes.upsa.edu.bo>
2020-09-12 11:56:23 -0400

    creacion del MVC

---
+++ ProductEnum/ProductEnum/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductEnum.Models
+{
+
+    public enum CategoryType
+    {
+        Electronicos=10,
+        Alimenticios=20,
+        Moda=30,
+        Belleza=40,
+        Automotriz=50,
+        libros=60
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+       
+        [Display(Name ="Nombre del producto")]
+        [Required(ErrorMessage = "debe ingreasar el nombre del producto")]
+        public string Name { get; set; }
+
+        
+        [Display(Name = "Categoria del producto")]
+        [Required(ErrorMessage = "debe ingreasar la categoria del producto")]
+        public CategoryType Category { get; set; }
+    }
+}
